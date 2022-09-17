44aaf70104f7cc283fa0c9d816d97ebe5b00798b
MTSKaioken <kaio.martins2@aluno.unip.br>
2022-09-17 18:24:25 -0300

    componente Navbar em progresso

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <li><a :href="caminhoLink1">{{ link1 }}</a></li>
+      <li>{{ link2 }}</li>
+      <li>{{ link3 }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    link1: String,
+    caminhoLink1: String,
+    link2: String,
+    link3: String
+  }
+}
+</script>
+
+<style scoped>
+div ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+li {
+  display: inline;
+}
+</style>
--- frontend/src/views/teste/teste.vue
+++ frontend/src/views/teste/teste.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-  teste sadasdsa
+    <Navbar caminho-link1="/" link1="seila" />
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
-  name: 'teste'
+  name: 'teste',
+  components: {
+    Navbar
+  }
 }
 </script>
 
